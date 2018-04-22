941b5fbf84f105294c557b0067644f7a5ed543aa
Zach Shen <zshen@videye.net>
2018-04-22 13:51:13 -0700

    Add main() function to example_usage.py

--- example_usage.py
+++ example_usage.py
@@ -1,19 +1,26 @@
+#!/usr/bin/env python
+
 from redfin import RedFin
 from house import House
 from listing import Listing
 
-redfin = RedFin()
-redfin.use_proxies = False
-redfin.get_search_results()
+def main():
+    redfin = RedFin()
+    redfin.use_proxies = False
+    redfin.get_search_results()
 
-r_data = redfin.get_one_property_data()
-h = House(street_address = r_data['street_address'],
-          city = r_data['address_locality'],
-          state = r_data['address_region'],
-          zip_code = r_data['postal_code'],
-          beds = r_data['beds'],
-          baths = r_data['baths'],
-          sq_ft = r_data['sqFt'],
-          lot_size = None,
-          home_type = 'sfh')
-l = Listing(house = h)
+    r_data = redfin.get_one_property_data()
+    h = House(street_address = r_data['street_address'],
+              city = r_data['address_locality'],
+              state = r_data['address_region'],
+              zip_code = r_data['postal_code'],
+              beds = r_data['beds'],
+              baths = r_data['baths'],
+              sq_ft = r_data['sqFt'],
+              lot_size = None,
+              home_type = 'sfh')
+    l = Listing(house = h)
+
+if __name__ == "__main__":
+    main()
+
